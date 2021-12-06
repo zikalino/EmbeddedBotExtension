ffc1f0311a40bc9b20452693d65de17c2b2b97db
Zim Kalinowski <zim.kalinowski@zoho.com>
2021-12-06 09:58:03 +0800

    version 0.0.3

--- src/extension.ts
+++ src/extension.ts
@@ -5,37 +5,43 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	let openWebview = vscode.commands.registerCommand('embeddedbot.helloWorld', () => {
-	  const panel = vscode.window.createWebviewPanel(
-	  'openWebview', // Identifies the type of the webview. Used internally
-	  'Example Page', // Title of the panel displayed to the user
-	  vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-	  { // Enable scripts in the webview
-		  enableScripts: true //Set this to true if you want to enable Javascript. 
-	  }
-	  );
+    let openWebview = vscode.commands.registerCommand('embeddedbot.helloWorld', () => {
 
-	  let secret = vscode.workspace.getConfiguration('embeddedview').get("secret");
-	  if (secret !== null && secret !== undefined) {
-	  	panel.webview.html = getWebviewContent(secret as string);
-	  } else {
-		vscode.window.showInputBox({ value: undefined, prompt: "Enter secret", placeHolder: undefined, password: true }).then((enteredSecret) => {
-			if (enteredSecret === undefined) {
-				return;
-			}
-			if (enteredSecret) {
-				secret = enteredSecret;
-				vscode.workspace.getConfiguration("embeddedview").update("secret", secret);
-			}
-			panel.webview.html = getWebviewContent(secret as string);
-		});	  }
+      let secret = vscode.workspace.getConfiguration('embeddedbot').get("secret");
+      if (secret !== null && secret !== undefined) {
+          createBot(secret as string);
+      } else {
+        vscode.window.showInputBox({ value: undefined, prompt: "Enter secret", placeHolder: undefined, password: true }).then((enteredSecret) => {
+            if (enteredSecret === undefined) {
+                return;
+            }
+            if (enteredSecret) {
+                secret = enteredSecret;
+                vscode.workspace.getConfiguration("embeddedbot").update("secret", secret, vscode.ConfigurationTarget.Global);
+            }
+            createBot(secret as string);
+        });   }
 
-	});
-	context.subscriptions.push(openWebview);
+    });
+    context.subscriptions.push(openWebview);
   }
   
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+
+function createBot(secret: string) {
+    const panel = vscode.window.createWebviewPanel(
+        'openWebview', // Identifies the type of the webview. Used internally
+        'Embedded Bot', // Title of the panel displayed to the user
+        vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+        { // Enable scripts in the webview
+            enableScripts: true //Set this to true if you want to enable Javascript. 
+        }
+        );
+
+        panel.webview.html = getWebviewContent(secret as string);  
+}
 
 function getWebviewContent(secret: string) {
   return `<!DOCTYPE html>
@@ -46,7 +52,7 @@ function getWebviewContent(secret: string) {
     <title>Example Webview</title>
 </head>
 <body>
-	<iframe src='https://webchat.botframework.com/embed/codebotx?s=` + secret + `' height="1024" width="100%"></iframe>
+    <iframe src='https://webchat.botframework.com/embed/codebotx?s=` + secret + `' height="764" width="100%"></iframe>
 </body>
 </html>`;
 }
